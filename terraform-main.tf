953286d738c69af765441619b5c9156d045b6ad7
kenttannady <kenttannady@yahoo.co.id>
2025-05-28 22:44:19 +0700

    Create terraform-main.tf

---
+++ terraform-main.tf
@@ -0,0 +1,22 @@
+# main.tf
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Web-Server-Auto"
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name = "allow-http"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
